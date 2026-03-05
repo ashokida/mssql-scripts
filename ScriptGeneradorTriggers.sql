c2dcaaf5738c20777704b2c150c6ed94014f2802
ashokida <shoki3000@gmail.com>
2026-03-05 15:40:21 -0300

    Delete ScriptGeneradorTriggers.sql

--- ScriptGeneradorTriggers.sql
+++
@@ -1,8 +0,0 @@
-
- SELECT --DB_NAME() AS DataBaseName,                  
-        --dbo.SysObjects.Name AS TriggerName,
-          dbo.sysComments.Text AS SqlContent
-   FROM   dbo.SysObjects INNER JOIN dbo.sysComments ON dbo.SysObjects.ID = dbo.sysComments.ID
-  WHERE  (dbo.SysObjects.xType = 'TR') 
-  --AND   dbo.SysObjects.Name = '<YourTriggerName>'
-ORDER BY  dbo.SysObjects.Name
